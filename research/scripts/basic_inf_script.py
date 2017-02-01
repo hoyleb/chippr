cc35d211afda2651f6a81284247fe0c6ed3b9ca2
aimalz <aimalz@nyu.edu>
2017-02-01 18:43:11 -0500

    inference script now supports parallelization

--- research/scripts/basic_inf_script.py
+++ research/scripts/basic_inf_script.py
@@ -15,7 +15,36 @@ def make_true():
 
     return(true_nz)
 
-def the_loop(true_nz=None):
+def set_up_prior(data):
+    """
+    Function to create prior distribution from data
+
+    Parameters
+    ----------
+    data: dict
+        catalog dictionary containing bin endpoints, log interim prior, and log interim posteriors
+
+    Returns
+    -------
+    prior: chippr.mvn object
+        prior distribution as multivariate normal
+    """
+    zs = data['bin_ends']
+    log_nz_intp = np.exp(data['log_interim_prior'])
+    log_z_posts = np.exp(data['log_interim_posteriors'])
+
+    z_difs = zs[1:]-zs[:-1]
+    z_mids = (zs[1:]+zs[:-1])/2.
+    n_bins = len(z_mids)
+
+    prior_var = np.eye(n_bins)
+    for k in range(n_bins):
+        prior_var[k] = 1. * np.exp(-0.5 * (z_mids[k] - z_mids) ** 2 / 0.05 ** 2)
+    prior_mean = log_nz_intp
+    prior = mvn(prior_mean, prior_var)
+    return prior
+
+def the_loop(given_key):
     """
     Function to create a catalog once true redshifts exist
 
@@ -24,56 +53,54 @@ def the_loop(true_nz=None):
     true_nz: chippr.gmix object, optional
         gaussian mixture probability distribution
     """
-    result_dir = os.path.join('..', 'results')
-    name_file = 'which_inf_tests.txt'
+    test_info = all_tests[given_key]
+    test_name = test_info['name']
+    true_nz = test_info['truth']
 
-    with open(name_file) as tests_to_run:
+    test_name = test_name[:-1]
+    param_file_name = test_name + '.txt'
 
-        for test_name in tests_to_run:
+    test_dir = os.path.join(result_dir, test_name)
+    simulated_posteriors = catalog(params=param_file_name, loc=test_dir)
+    saved_location = 'data'
+    saved_type = '.txt'
+    data = simulated_posteriors.read(loc=saved_location, style=saved_type)
 
-            test_name = test_name[:-1]
-            param_file_name = test_name + '.txt'
+    prior = set_up_prior(data)
 
-            test_dir = os.path.join(result_dir, test_name)
-            simulated_posteriors = catalog(params=param_file_name, loc=test_dir)
-            saved_location = 'data'
-            saved_type = '.txt'
-            data = simulated_posteriors.read(loc=saved_location, style=saved_type)
+    nz = log_z_dens(data, prior, truth=true_nz, loc=test_dir, vb=True)
 
-            zs = data['bin_ends']
-            nz_intp = np.exp(data['log_interim_prior'])
-            z_posts = np.exp(data['log_interim_posteriors'])
+    nz_stacked = nz.calculate_stacked()
+    nz_mmap = nz.calculate_mmap()
+    nz_mexp = nz.calculate_mexp()
+    nz_stats = nz.compare()
 
-            z_difs = zs[1:]-zs[:-1]
-            z_mids = (zs[1:]+zs[:-1])/2.
-            n_bins = len(z_mids)
+    nz.plot_estimators()
 
-            prior_var = np.eye(n_bins)
-            for k in range(n_bins):
-                prior_var[k] = 1. * np.exp(-0.5 * (z_mids[k] - z_mids) ** 2 / 0.05 ** 2)
-            prior_mean = nz_intp
-            prior = mvn(prior_mean, prior_var)
-
-            nz = log_z_dens(data, prior, truth=true_nz, loc=test_dir, vb=True)
-
-            nz_stacked = nz.calculate_stacked()
-            nz_mmap = nz.calculate_mmap()
-            nz_mexp = nz.calculate_mexp()
-            nz_stats = nz.compare()
-
-            nz.info['estimators'].keys()
-            nz.plot_estimators('final_plot.png')
-            
-            nz.write('nz.p')
+    nz.write('nz.p')
 
 if __name__ == "__main__":
 
     import numpy as np
     import os
+    import multiprocessing as mp
 
     import chippr
     from chippr import *
 
     true_nz = make_true()
 
-    the_loop(true_nz=true_nz)
+    result_dir = os.path.join('..', 'results')
+    name_file = 'which_inf_tests.txt'
+
+    with open(name_file) as tests_to_run:
+        all_tests = {}
+        for test_name in tests_to_run:
+            test_info = {}
+            test_info['name'] = test_name
+            test_info['truth'] = true_nz
+            all_tests[test_name] = test_info
+
+    nps = mp.cpu_count()-1
+    pool = mp.Pool(nps)
+    pool.map(the_loop, all_tests.keys())
