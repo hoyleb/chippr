030d0ab6972bc45e8c922c7425ab93606bf7cab1
aimalz <aimalz@nyu.edu>
2017-02-23 15:47:29 -0500

    moving functions around

--- chippr/utils.py
+++ chippr/utils.py
@@ -27,3 +27,25 @@ def safe_log(arr, threshold=d.eps):
     flat = arr.flatten()
     logged = np.log(np.array([max(a,threshold) for a in flat])).reshape(shape)
     return logged
+
+def ingest(in_info):
+    """
+    Function reading in parameter file to define functions necessary for generation of posterior probability distributions
+
+    Parameters
+    ----------
+    in_info: string or dict
+        string containing path to plaintext input file or dict containing likelihood input parameters
+
+    Returns
+    -------
+    in_dict: dict
+        dict containing keys and values necessary for posterior probability distributions
+    """
+    if type(in_info) == str:
+        with open(in_info) as infile:
+            lines = (line.split(None) for line in infile)
+            in_dict = {defn[0] : defn[1:] for defn in lines}
+    else:
+        in_dict = in_info
+    return in_dict
