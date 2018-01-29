02dcecb0b5e36796edb8c8532cab016c3ae1b94b
Alex Malz <aimalz@nyu.edu>
2018-01-29 12:49:32 -0500

    forgot a new file

---
+++ chippr/gamma.py
@@ -0,0 +1,103 @@
+# Wrapper for pomegranate.distributions.GammaDistribution
+
+import sys
+
+import numpy as np
+
+from pomegranate.distributions import GammaDistribution as GD
+
+import chippr
+
+class gamma(object):
+
+    def __init__(self, shape, ratesqr):
+        """
+        A univariate Gaussian probability distribution object
+
+        Parameters
+        ----------
+        shape: float
+            shape parameter of gamma probability distribution
+        ratesqr: float
+            square of rate parameter of gamma probability distribution
+        """
+        self.shape = shape
+        self.rate = np.sqrt(ratesqr)
+        self.dist = GD(self.shape, self.rate)
+
+    def pdf(self, xs):
+        return self.evaluate(xs)
+
+    def evaluate_one(self, x):
+        """
+        Function to evaluate Gaussian probability distribution once
+
+        Parameters
+        ----------
+        x: float
+            value at which to evaluate Gaussian probability distribution
+
+        Returns
+        -------
+        p: float
+            probability associated with x
+        """
+        # p = 1. / (np.sqrt(2. * np.pi) * self.sigma) * \
+        # np.exp(-0.5 * (self.mean - x) * self.invvar * (self.mean - x))
+        p = self.dist.probability(x)
+        return p
+
+    def evaluate(self, xs):
+        """
+        Function to evaluate univariate Gaussian probability distribution at multiple points
+
+        Parameters
+        ----------
+        xs: numpy.ndarray, float
+            input values at which to evaluate probability
+
+        Returns
+        -------
+        ps: ndarray, float
+            output probabilities
+        """
+        # ps = 1. / (np.sqrt(2. * np.pi) * self.sigma) * \
+        # np.exp(-0.5 * (self.mean - xs) * self.invvar * (self.mean - xs))
+        # ps = np.zeros_like(xs)
+        # for n, x in enumerate(xs):
+        #     ps[n] += self.evaluate_one(x)
+        ps = self.dist.probability(xs)
+        return ps
+
+    def sample_one(self):
+        """
+        Function to take one sample from univariate Gaussian probability distribution
+
+        Returns
+        -------
+        x: float
+            single sample from Gaussian probability distribution
+        """
+        # x = self.mean + self.sigma * np.random.normal()
+        x = self.dist.sample(1)
+        return x
+
+    def sample(self, n_samps):
+        """
+        Function to sample univariate Gaussian probability distribution
+
+        Parameters
+        ----------
+        n_samps: positive int
+            number of samples to take
+
+        Returns
+        -------
+        xs: ndarray, float
+            array of n_samps samples from Gaussian probability distribution
+        """
+        # print('gauss trying to sample '+str(n_samps)+' from '+str(self.dist))
+        # xs = np.array([self.sample_one() for n in range(n_samps)])
+        xs = np.array(self.dist.sample(n_samps))
+        # print('gauss sampled '+str(n_samps)+' from '+str(self.dist))
+        return xs
